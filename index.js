36008ee30012115003ed10a6397fe4c88054d4e3
Stanislava Koklina <medula_oblomgata@mail.ru>
2017-10-19 12:09:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function getFirstSelector(selector) {
-  return document.querySelector(selector);
+function getFirstSelector('li') {
+  return document.querySelector('li');
 }
 
 function nestedTarget() {
