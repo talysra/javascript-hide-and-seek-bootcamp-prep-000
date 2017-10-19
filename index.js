1c1464c45c3ca888f58e7c2000377360cde86619
Stanislava Koklina <medula_oblomgata@mail.ru>
2017-10-19 12:23:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function nestedTarget() {
 }
 
 function increaseRankBy(n) {
-  const lis = document.getElementNyId('app').querySelectorAll('ul.ranked-list li');
+  const lis = document.getElementById('app').querySelectorAll('ul.ranked-list li');
 
   for (let i = 0; i < lis.length; i++) {
     lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
